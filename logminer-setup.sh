dc650aaf58bbaf4e6511a977fcb76e9eb775d235
Anuj Kumar <anuj.kumar@disqo.com>
2024-09-27 22:43:00 +0530

    Initial

--- logminer-setup.sh
+++ logminer-setup.sh
@@ -25,16 +25,19 @@ EOF
 
 # Create Log Miner Tablespace and User
 sqlplus sys/password@//localhost:1521/FREE as sysdba <<- EOF
+  alter session set container=ORACLEDB;
   CREATE TABLESPACE LOGMINER_TBS DATAFILE '/opt/oracle/oradata/FREE/logminer_tbs.dbf' SIZE 25M REUSE AUTOEXTEND ON MAXSIZE UNLIMITED;
   exit;
 EOF
 
 sqlplus sys/password@//localhost:1521/FREEPDB1 as sysdba <<- EOF
+  alter session set container=ORACLEDB;
   CREATE TABLESPACE LOGMINER_TBS DATAFILE '/opt/oracle/oradata/FREE/FREEPDB1/logminer_tbs.dbf' SIZE 25M REUSE AUTOEXTEND ON MAXSIZE UNLIMITED;
   exit;
 EOF
 
 sqlplus sys/password@//localhost:1521/FREE as sysdba <<- EOF
+  alter session set container=ORACLEDB;
   CREATE USER c##dbzuser IDENTIFIED BY dbz DEFAULT TABLESPACE LOGMINER_TBS QUOTA UNLIMITED ON LOGMINER_TBS CONTAINER=ALL;
 
   GRANT CREATE SESSION TO c##dbzuser CONTAINER=ALL;
