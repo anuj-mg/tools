c9da09f9d6eeac30e1bee1282f268e4096591f61
Anuj Kumar <anuj.kumar@disqo.com>
2024-09-29 17:44:20 +0530

    Initial

--- logminer-setup.sh
+++ logminer-setup.sh
@@ -37,7 +37,7 @@ sqlplus sys/password@//localhost:1521/FREEPDB1 as sysdba <<- EOF
 EOF
 
 sqlplus sys/password@//localhost:1521/FREE as sysdba <<- EOF
-  alter session set container = CDB$ROOT
+  alter session set container = CDB$ROOT;
   CREATE USER c##dbzuser IDENTIFIED BY dbz DEFAULT TABLESPACE LOGMINER_TBS QUOTA UNLIMITED ON LOGMINER_TBS CONTAINER=ALL;
   GRANT CREATE SESSION TO c##dbzuser CONTAINER=ALL;
   GRANT SET CONTAINER TO c##dbzuser CONTAINER=ALL;
